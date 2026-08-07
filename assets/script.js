9733fc5f03cf8c0cd147f1a1225d7414ed1927cd
ContreEvidence <contreevidence@gmail.com>
2026-08-07 16:31:15 +0200

    Force la navigation v24 avec Hors Cadre

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,9 @@
   'use strict';
   const currentScript = document.currentScript;
   if (!document.documentElement.dataset.ceNavLoader) {
-    document.documentElement.dataset.ceNavLoader = '23';
+    document.documentElement.dataset.ceNavLoader = '24';
     const navScript = document.createElement('script');
-    navScript.src = new URL('navigation-v3.js?v=20260807-23', currentScript?.src || window.location.href).href;
+    navScript.src = new URL('navigation-v3.js?v=20260807-24', currentScript?.src || window.location.href).href;
     navScript.defer = true;
     document.head.appendChild(navScript);
   }
