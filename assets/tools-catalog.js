095c5ef1ea9219c0e9db17a6920c366be332a17e
ContreEvidence <contreevidence@gmail.com>
2026-08-08 16:51:25 +0200

    Add tools catalog with mortgage simulator

---
+++ assets/tools-catalog.js
@@ -0,0 +1,3 @@
+window.CE_TOOLS_CATALOG = [
+  {d:'patrimoine',t:'outil',c:'Crédit immobilier',h:'simulateur-capacite-emprunt.html',n:'Simulateur de capacité d’emprunt immobilier',x:'Estimer mensualité maximale, capital empruntable et sensibilité aux taux à partir des règles HCSF et d’un taux de référence Banque de France modifiable.',k:'simulateur outil crédit immobilier emprunt capacité mensualité taux banque apport hcsf'}
+];
