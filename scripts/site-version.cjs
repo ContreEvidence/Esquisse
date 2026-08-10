42a233aed6c5490d00370d8fd793a0834cb30fda
ContreEvidence <contreevidence@gmail.com>
2026-08-10 22:24:01 +0200

    Forcer la consolidation finale du cockpit immobilier

--- scripts/site-version.cjs
+++ scripts/site-version.cjs
@@ -2,4 +2,4 @@
 
 // Version unique des couches front injectées par le pipeline.
 // Toute nouvelle consolidation importante incrémente cette valeur une seule fois.
-module.exports = '20260810-11';
+module.exports = '20260810-12';
