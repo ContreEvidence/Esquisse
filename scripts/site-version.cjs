280b7f109d0ca3d260f39e72aeeb81df58977c88
ContreEvidence <contreevidence@gmail.com>
2026-08-10 20:06:28 +0200

    Centraliser la version des couches front

---
+++ scripts/site-version.cjs
@@ -0,0 +1,5 @@
+'use strict';
+
+// Version unique des couches front injectées par le pipeline.
+// Toute nouvelle consolidation importante incrémente cette valeur une seule fois.
+module.exports = '20260810-10';
